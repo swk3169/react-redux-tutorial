f643ad04b362a3624e6d797bdca17d3131725b0d
swk3169 <swk3169@gmail.com>
2018-08-04 00:25:46 +0900

    ver0.02
    redux-actions의 createAction과 handleActions 사용

--- src/store/modules/counter.js
+++ src/store/modules/counter.js
@@ -1,28 +1,27 @@
 // 카운터 관련 상태 로직
+import { createAction } from 'redux-actions';
+
 // 액션 타임을 정의해준다.
 const INCREMENT = 'counter/INCREMENT';
 const DECREMENT = 'counter/DECREMENT';
 
 // 액션 생성 함수를 만든다.
 // 이 함수들은 나중에 다른파일에서 불러와야 하므로 내보내준다.
-export const increment = () => ({ type: INCREMENT });
-export const decrement = () => ({ tpye: DECREMENT });
+export const increment = createAction(INCREMENT);
+export const decrement = createAction(DECREMENT);
 
 // 모듈의 초기 상태를 정의한다.
 const initialState = {
     number: 0
 };
 
-// 리듀서를 만들어서 내보내준다.
-export default function reducer(state = initialState, action) {
-    // 리듀서 함수에서는 액션의 타임에 따라 변화된 상태를 정의하여 반환한다.
-    // state = initialState 이렇게 하면 initialState가 기본 값으로 사용된다.
-    switch(action.type) {
-        case INCREMENT:
-            return { number: state.number + 1 };
-        case DECREMENT:
-            return { number: state.number - 1 };
-        default:
-            return state;   // 아무 일도 일어나지 않으면 현재 상태를 그대로 반환한다.
-    }
-}
+// handleActions의 첫번째 파라미터는 액션을 처리하는 함수들로 이뤄진 객체이고
+// 두번째 파라미터는 초기 상태이다.
+export default handleActions({
+    [INCREMENT]: (state, action) => {
+        return { number: state.number + 1 };
+    },
+    // action 객체를 참조하지 않기 때문에 이렇게 생략 가능
+    // state 부분에서 비구조화 할당을 해주었기 때문에 코드가 더욱 간소화 됨
+    [DECREMENT]: ({ number }) => ({ number: number - 1 })
+}, initialState);
