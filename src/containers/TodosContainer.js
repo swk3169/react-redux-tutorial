07e1ee5d41704d2a0ecc0778d49dca2bea271b59
swk3169 <swk3169@gmail.com>
2018-08-06 20:51:09 +0900

    ver0.05
    record 추가

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -4,6 +4,7 @@ import Todos from 'components/Todos';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { TodoActions } from 'store/actionCreators';
+import * as todoActions from 'store/modules/todo';
 
 class TodosContainer extends Component {
     handleChange = (e) => {
@@ -50,5 +51,8 @@ export default connect(
         // immutable을 사용하니, 값을 조회 할때에는 .get을 사용하여야 한다.
         input: todo.input,
         todos: todo.todos
+    }),
+    (dispatch) => ({
+        TodoActions: bindActionCreators(todoActions, dispatch)
     })
 )(TodosContainer);
--- src/store/modules/todo.js
+++ src/store/modules/todo.js
@@ -1,5 +1,5 @@
 import { createAction, handleActions } from 'redux-actions';
-import { Map, List } from 'immutable';
+import { Record, List } from 'immutable';
 
 const CHANGE_INPUT = 'todo/CHANGE_INPUT';
 const INSERT = 'todo/INSERT';
@@ -15,24 +15,26 @@ let id = 0; // todo 아이템에 들어갈 고유 값
 
 // Record 함수는 Record 형태 데이터를 만드는 함수를 반환한다.
 // 따라서, 만든 다음에 뒤에 ()를 붙여줘야 데이터가 생성된다.
-const initialState = Map({
+const initialState = Record({
     input: '',
     todos: List()
-});
+})();
 
-// // Todo 아이템의 형식을 정한다.
-// const TodoRecord = Record({
-//     id: id++,
-//     text: '',
-//     checked: false
-// })
+// Todo 아이템의 형식을 정한다.
+const TodoRecord = Record({
+    id: id++,
+    text: '',
+    checked: false
+})
 
 export default handleActions({
     // 한줄짜리 코드로 반환 할 수 있는 경우엔 다음과 같이 블록 { } 를 생략 할 수 있다.
     [CHANGE_INPUT]: (state, action) => state.set('input', action.payload),
     [INSERT]: (state, { payload: text }) => {
         // 위 코드는 action 객체를 비구조화 할당하고, payload 값을 text라고 부르겠다는 의미이다.
-        const item = Map({ id: id++, checked: false, text }); // 하나 추가 할 때마다 id 값을 증가시킨다.
+        // TodoRecord를 사용해야 아이템도 Record 형식으로 조회 가능하다.
+        // 빠져있는 값은, 기본값을 사용하게 된다. (checked: false)
+        const item = TodoRecord({ id: id++, text }); // 하나 추가 할 때마다 id 값을 증가시킨다.
         return state.update('todos', todos => todos.push(item));
     },
     [TOGGLE]: (state, { payload: id }) => {
